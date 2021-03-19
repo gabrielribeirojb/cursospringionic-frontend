bc3f63f7b4b007c149243a3d7031b74315a744bd
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-03-19 19:44:30 -0300

    Informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -51,6 +52,10 @@ export class OrderConfirmationPage {
   total() : number {
     return this.cartService.total();
   }
+
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
   
   back() {
     this.navCtrl.setRoot('CartPage');
@@ -60,11 +65,16 @@ export class OrderConfirmationPage {
     this.pedidoService.insert(this.pedido)
     .subscribe(response => {
       this.cartService.createOrClearCart();
-      console.log(response.headers.get('location'));
+      this.codpedido = this.extractId(response.headers.get('location'));
     }, error => {
       if(error.status == 403){
         this.navCtrl.setRoot('HomePage');
       }
     })
+  }
+
+  private extractId(location: string): string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 }
